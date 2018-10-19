5e0e4559960503d373c202ef6ab49c84b2d1057f
ruslanMacari <rusmacari@gmail.com>
2018-10-19 07:20:22 +0300

    test login controller

--- src/test/java/ruslan/macari/controllers/LoginControllerTest.java
+++ src/test/java/ruslan/macari/controllers/LoginControllerTest.java
@@ -1,8 +1,9 @@
 package ruslan.macari.controllers;
 
+import javax.servlet.http.HttpSession;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.doCallRealMethod;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -12,16 +13,21 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.verify;
+import org.springframework.ui.Model;
+import ruslan.macari.domain.UserLogin;
 import ruslan.macari.service.UserService;
 
 public class LoginControllerTest {
     
-    private final MockHttpSession session;
-    private final LoginController loginController;
+    private MockHttpSession session;
+    private LoginController loginController;
+    private UserService userService;
     
     public LoginControllerTest() {
         session = new MockHttpSession();
         loginController = new LoginController();
+        userService = mock(UserService.class);
+        loginController.setUserService(userService);
     }
     
     @Test
@@ -43,9 +49,7 @@ public class LoginControllerTest {
     @Test
     public void testSaveUser() {
         BindingResult bindingResult = mock(BindingResult.class);
-        User user = new User();
-        UserService userService = mock(UserService.class);
-        loginController.setUserService(userService);
+        User user = mock(User.class);
         when(bindingResult.hasErrors()).thenReturn(true);
         String result = loginController.saveUser(user, bindingResult);
         assertEquals(result, "createUser");
@@ -53,32 +57,26 @@ public class LoginControllerTest {
         when(bindingResult.hasErrors()).thenReturn(false);
         result = loginController.saveUser(user, bindingResult);
         assertEquals(result, "redirect:/login");
-        verify(userService, Mockito.times(1));
+        verify(userService).addUser(user);
     }
-//
-//    @Test
-//    public void testLogin() {
-//        System.out.println("login");
-//        Model model = null;
-//        LoginController instance = new LoginController();
-//        String expResult = "";
-//        String result = instance.login(model);
-//        assertEquals(expResult, result);
-//        fail("The test case is a prototype.");
-//    }
-//
-//    @Test
-//    public void testCheckUser() {
-//        System.out.println("checkUser");
-//        UserLogin user = null;
-//        BindingResult result_2 = null;
-//        Model model = null;
-//        HttpSession session = null;
-//        LoginController instance = new LoginController();
-//        String expResult = "";
-//        String result = instance.checkUser(user, result_2, model, session);
-//        assertEquals(expResult, result);
-//        fail("The test case is a prototype.");
-//    }
+
+    @Test
+    public void testLogin() {
+        String result = loginController.login(mock(Model.class));
+        assertEquals(result, "login");
+    }
+
+    @Test
+    public void testCheckUser() {
+        UserLogin user = mock(UserLogin.class);
+        BindingResult bindingResult = mock(BindingResult.class);
+        when(bindingResult.hasErrors()).thenReturn(true);
+        Model model = mock(Model.class);
+        String result = loginController.checkUser(user, bindingResult, model, session);
+        assertEquals(result, "login");
+        when(bindingResult.hasErrors()).thenReturn(false);
+        result = loginController.checkUser(user, bindingResult, model, session);
+        assertEquals(result, "redirect:/home");
+    }
     
 }
