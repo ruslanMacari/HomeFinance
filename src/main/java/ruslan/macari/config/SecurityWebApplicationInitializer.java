efe6b89d9019525021259cbd0f5218e7dd46eb3f
ruslanMacari <rusmacari@gmail.com>
2018-11-19 18:00:13 +0200

    added security config

---
+++ src/main/java/ruslan/macari/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package ruslan.macari.config;
+
+import org.springframework.security.web.context.*;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
--- src/main/java/ruslan/macari/config/WebAppInitializer.java
+++ src/main/java/ruslan/macari/config/WebAppInitializer.java
@@ -9,7 +9,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
    @Override
    protected Class<?>[] getRootConfigClasses() {
-      return new Class[] {AppConfig.class};
+      return new Class[] {AppConfig.class, SecurityConfig.class};
    }
 
    @Override
