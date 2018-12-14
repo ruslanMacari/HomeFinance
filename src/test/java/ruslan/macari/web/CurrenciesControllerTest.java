3f8bee02d16694df6e5e16a60654ec6f0748323b
ruslanMacari <rusmacari@gmail.com>
2018-12-14 14:44:50 +0200

    add currenciesController test

---
+++ src/test/java/ruslan/macari/web/CurrenciesControllerTest.java
@@ -0,0 +1,27 @@
+package ruslan.macari.web;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.springframework.ui.Model;
+import ruslan.macari.service.CurrencyService;
+
+public class CurrenciesControllerTest {
+    
+    private CurrencyService currencyService;
+    private CurrenciesController controller;
+    private Model model;
+    
+    public CurrenciesControllerTest() {
+        currencyService = mock(CurrencyService.class);
+        controller = new CurrenciesController();
+        controller.setCurrencyService(currencyService);
+        model = mock(Model.class);
+    }
+    
+    @Test
+    public void testList() {
+        assertTrue(controller.list(model).equals("currencies/list"));
+    }
+    
+}
