45d68252b116ac75c9207fe3fb5638c30f8972e1
RMACARI <ruslan.macari@orange.md>
2019-12-02 17:11:55 +0200

    Added Status Code to common Error page

--- src/main/java/homefinance/web/exceptionshandle/GlobalErrorController.java
+++ src/main/java/homefinance/web/exceptionshandle/GlobalErrorController.java
@@ -3,21 +3,24 @@ package homefinance.web.exceptionshandle;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GlobalErrorController implements ErrorController {
 
   @RequestMapping("/error")
-  public void handleError(HttpServletRequest request) throws Throwable {
-    Object error = request.getAttribute("javax.servlet.error.exception");
-    if (error != null) {
-      throw (Throwable) error;
-    }
+  public String handleError(HttpServletRequest request, Model model) {
+    Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+    Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+    model.addAttribute("errorMessage", exception);
+    model.addAttribute("statusCode", statusCode);
+    return "exception";
+
   }
 
   @Override
   public String getErrorPath() {
-    return null;
+    return "/error";
   }
 }
