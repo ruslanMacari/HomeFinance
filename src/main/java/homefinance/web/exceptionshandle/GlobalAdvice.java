5fe80df58261f102f36c9f39b3007df6b541d7ee
RMACARI <ruslan.macari@orange.md>
2019-12-03 09:38:31 +0200

    Migration to thymeleaf resource-not-found page, added 404 error handling in globalErrorController, added status code in GlobalAdvice for exception page

--- src/main/java/homefinance/web/exceptionshandle/GlobalAdvice.java
+++ src/main/java/homefinance/web/exceptionshandle/GlobalAdvice.java
@@ -1,8 +1,10 @@
 package homefinance.web.exceptionshandle;
 
+import javax.servlet.http.HttpServletRequest;
 import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,11 +17,20 @@ public class GlobalAdvice {
     private final Logger logger = LoggerFactory.getLogger(GlobalAdvice.class.getName());
     
     @ExceptionHandler(Throwable.class)
-    public String exception(Exception ex, Model model) {
+    public String exception(HttpServletRequest request, Exception ex, Model model) {
         String error = Jsoup.parse(ex.getMessage()).text();
         logger.error(error, ex);
         model.addAttribute("errorMessage", error);
+        model.addAttribute("statusCode", this.getStatus(request));
         return "exception";
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
     }
     
     @ExceptionHandler({NoHandlerFoundException.class, PageNotFoundException.class})
--- src/main/java/homefinance/web/exceptionshandle/GlobalErrorController.java
+++ src/main/java/homefinance/web/exceptionshandle/GlobalErrorController.java
@@ -1,7 +1,9 @@
 package homefinance.web.exceptionshandle;
 
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,10 @@ public class GlobalErrorController implements ErrorController {
     Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
     model.addAttribute("errorMessage", exception);
     model.addAttribute("statusCode", statusCode);
+    if (Objects.nonNull(statusCode)
+      && HttpStatus.NOT_FOUND.value() == statusCode) {
+      return "resource-not-found";
+    }
     return "exception";
 
   }
