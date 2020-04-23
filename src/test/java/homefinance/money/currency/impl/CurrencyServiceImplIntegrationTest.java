f073b476dc08d4a3d6ae9e1b448153511d95d7a2
ruslanMacari <rusmacari@gmail.com>
2020-04-23 17:04:13 +0300

    heroku fix deploy added @AutoConfigureTestDatabase

--- src/test/java/homefinance/money/currency/impl/CurrencyServiceImplIntegrationTest.java
+++ src/test/java/homefinance/money/currency/impl/CurrencyServiceImplIntegrationTest.java
@@ -20,11 +20,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
+@AutoConfigureTestDatabase
 public class CurrencyServiceImplIntegrationTest {
 
   @Autowired
