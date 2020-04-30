630773e9a39fcaae3d3cd40a950cbe8d6ca53ff9
ruslanMacari <rusmacari@gmail.com>
2020-04-30 19:16:12 +0300

    configure embedded pg

--- src/test/java/homefinance/TestConfiguration.java
+++ src/test/java/homefinance/TestConfiguration.java
@@ -27,7 +27,7 @@ public class TestConfiguration {
   @Bean
   public Consumer<Builder> embeddedPostgresCustomizer(
       @Value("${embedded-pg.directory}") String directory) {
-    return builder -> builder.setServerConfig("ssl", "disable")
+    return builder -> builder.setServerConfig("ssl", "false")
         .setOverrideWorkingDirectory(new File(directory));
         //.setConnectConfig("sslfactory", "org.postgresql.ssl.NonValidatingFactory");
         //.setServerConfig("sslmode", "disable");
