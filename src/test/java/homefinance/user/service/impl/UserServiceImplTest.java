c65f974f6621021527ded4424a56b68d54f74a1c
ruslanMacari <rusmacari@gmail.com>
2020-07-13 06:55:30 +0300

    -removed unnecessary 'this.'

--- src/test/java/homefinance/user/service/impl/UserServiceImplTest.java
+++ src/test/java/homefinance/user/service/impl/UserServiceImplTest.java
@@ -16,34 +16,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class UserServiceImplTest {
 
   private UserServiceImpl userService;
-  private String rootname = "root";
-  private String rootpassword = "pass";
+  private final String rootname = "root";
+  private final String rootpassword = "pass";
   private PasswordEncoder encoder;
   private User user;
 
   @Before
   public void setUp() {
-    this.userService = new UserServiceImpl();
-    this.user = mock(User.class);
-    this.userService.setRootName(this.rootname);
-    this.userService.setRootPassword(this.rootpassword);
-    this.encoder = mock(PasswordEncoder.class);
-    this.userService.setEncoder(this.encoder);
+    userService = new UserServiceImpl();
+    user = mock(User.class);
+    userService.setRootName(rootname);
+    userService.setRootPassword(rootpassword);
+    encoder = mock(PasswordEncoder.class);
+    userService.setEncoder(encoder);
   }
 
   @Test
   public void testInit() {
     ConstraintPersist constraintPersist = mock(ConstraintPersist.class);
-    this.userService.setConstraintPersist(constraintPersist);
+    userService.setConstraintPersist(constraintPersist);
     UserRepository repository = mock(UserRepository.class);
-    this.userService.setUserRepository(repository);
-    when(repository.findByName(this.rootname)).thenReturn(this.user);
-    this.userService.init();
+    userService.setUserRepository(repository);
+    when(repository.findByName(rootname)).thenReturn(user);
+    userService.init();
     verify(constraintPersist, times(0)).add(any(), any());
-    when(repository.findByName(this.rootname)).thenReturn(null);
-    when(this.encoder.encode(this.rootpassword)).thenReturn(this.rootpassword);
-    this.userService.init();
-    verify(this.encoder, times(1)).encode(this.rootpassword);
+    when(repository.findByName(rootname)).thenReturn(null);
+    when(encoder.encode(rootpassword)).thenReturn(rootpassword);
+    userService.init();
+    verify(encoder, times(1)).encode(rootpassword);
     verify(constraintPersist, times(1)).add(any(), any());
   }
 
