d18d0c21eb1c2a74026a92a475683fa7c3af4a1b
ruslanMacari <rusmacari@gmail.com>
2020-09-11 17:39:46 +0300

    Refactored HomeController + test

--- src/main/java/homefinance/home/HomeController.java
+++ src/main/java/homefinance/home/HomeController.java
@@ -1,6 +1,5 @@
 package homefinance.home;
 
-import homefinance.user.entity.User;
 import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,17 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class MainController {
+@RequestMapping(HomeController.URL)
+public class HomeController {
+  public static final String URL = "/";
+  public static final String USERNAME_ATTRIBUTE = "username";
 
   @GetMapping()
-  public String index() {
-    return "index";
+  public String home() {
+    return "home";
   }
 
   @GetMapping(value = "/access-denied")
   public String accessDenied(Principal user, Model model) {
-    model.addAttribute("user", new User(user.getName()));
+    model.addAttribute(USERNAME_ATTRIBUTE, user.getName());
     return "access-denied";
   }
 
---
+++ src/test/java/homefinance/home/HomeControllerTest.java
@@ -0,0 +1,44 @@
+package homefinance.home;
+
+import static homefinance.home.HomeController.USERNAME_ATTRIBUTE;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.security.Principal;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.springframework.ui.Model;
+
+public class HomeControllerTest {
+
+  public static final String USERNAME = "user";
+  private HomeController controller;
+
+  @Before
+  public void setUp() {
+    controller = new HomeController();
+  }
+
+  @Test
+  public void home_shouldReturnHomeViewName() {
+    // when:
+    String viewName = controller.home();
+    // then:
+    then(viewName).isEqualTo("home");
+  }
+
+  @Test
+  public void accessDenied_givenUser_shouldAddUserToModelAndReturnAccessDeniedViewName() {
+    // given:
+    Principal principalMock = mock(Principal.class);
+    given(principalMock.getName()).willReturn(USERNAME);
+    Model model = mock(Model.class);
+    // when:
+    String viewName = controller.accessDenied(principalMock, model);
+    // then:
+    then(viewName).isEqualTo("access-denied");
+    BDDMockito.then(model).should().addAttribute(USERNAME_ATTRIBUTE, USERNAME);
+  }
+}
