82a2321a98694f95e3cf0729b974f1363466994f
ruslan.macari <rusmacari@gmail.com>
2022-02-22 10:45:44 +0200

    Moved PasswordEncoder bean to SecurityConfig + minor refactoring

--- src/main/java/homefinance/common/config/security/SecurityConfig.java
+++ src/main/java/homefinance/common/config/security/SecurityConfig.java
@@ -1,41 +1,45 @@
 package homefinance.common.config.security;
 
-import homefinance.common.config.CommonBeansConfig;
 import homefinance.user.entity.Role;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
-@Import(CommonBeansConfig.class)
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserDetailsService userDetailsService;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   public SecurityConfig(@Qualifier("userDetailsService") UserDetailsService userDetailsService) {
     this.userDetailsService = userDetailsService;
   }
 
   @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder)
-      throws Exception {
-    auth.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder);
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder());
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().requireCsrfProtectionMatcher(this.getCsrfRequestMatcher()).disable();
+    http.csrf().requireCsrfProtectionMatcher(getCsrfRequestMatcher()).disable();
     http.authorizeRequests()
         .antMatchers("/assets/**", "/login*", "/login/**", "/rest/**").permitAll()
         .antMatchers("/users/**").hasAuthority(Role.ADMIN.name())
@@ -49,22 +53,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   private RequestMatcher getCsrfRequestMatcher() {
     return new RequestMatcher() {
 
-        // Disable CSFR protection on the following urls:
-        private AntPathRequestMatcher[] requestMatchers = {
-            new AntPathRequestMatcher("/rest/**")
-        };
-
-        @Override
-        public boolean matches(HttpServletRequest request) {
-          // If the request match one url the CSFR protection will be disabled
-          for (AntPathRequestMatcher rm : this.requestMatchers) {
-            if (rm.matches(request)) {
-              return true;
-            }
-          }
-          return false;
-        }
+      // Disable CSFR protection on the following urls:
+      private final AntPathRequestMatcher[] requestMatchers = {
+          new AntPathRequestMatcher("/rest/**")
       };
+
+      @Override
+      public boolean matches(HttpServletRequest request) {
+        // If the request match one url the CSFR protection will be disabled
+        for (AntPathRequestMatcher rm : requestMatchers) {
+          if (rm.matches(request)) {
+            return true;
+          }
+        }
+        return false;
+      }
+    };
   }
 
 }
