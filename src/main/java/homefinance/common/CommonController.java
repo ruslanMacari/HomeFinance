d961fc5db031673f59f50c18618e81168204d2c4
ruslan.macari <rusmacari@gmail.com>
2025-04-23 23:04:29 +0300

    Refactor to use Lombok annotations and improve logging
    Replaced explicit logger declarations with Lombok's `@Slf4j` for cleaner code. Simplified method calls by removing redundant `this` qualifiers and improved variable naming for clarity. Minor adjustments made to ensure consistency and code readability across classes.

--- src/main/java/homefinance/common/CommonController.java
+++ src/main/java/homefinance/common/CommonController.java
@@ -1,37 +1,37 @@
 package homefinance.common;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Component
+@Slf4j
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public abstract class CommonController<T> {
 
   public static final String REDIRECT = "redirect:";
   public static final String FLASH_MODEL_ATTRIBUTE_NAME = CommonController.class.getName() + '.' + "flashModel";
-  private static final Logger logger = LoggerFactory.getLogger(CommonController.class);
 
-  public static String getRedirectURL(String URL) {
-    return REDIRECT + URL;
+  public static String getRedirectURL(String url) {
+    return REDIRECT + url;
   }
 
   // TODO: 20.04.2020 RMACARI: move to a separate class? ex: FlashModel
   public static void addModelToRedirectAttributes(Model model,
       RedirectAttributes redirectAttributes) {
-    logger.debug("addModelToRedirectAttributes invoked");
+    log.debug("addModelToRedirectAttributes invoked");
     redirectAttributes.addFlashAttribute(FLASH_MODEL_ATTRIBUTE_NAME, model);
   }
 
   public static boolean isRedirectAndFlashModelMerged(Model model) {
-    Object flashModel = model.asMap().get(FLASH_MODEL_ATTRIBUTE_NAME);
-    if (flashModel == null) {
+    Object redirectFlashModelAttribute = model.asMap().get(FLASH_MODEL_ATTRIBUTE_NAME);
+    if (redirectFlashModelAttribute == null) {
       return false;
-    } else {
-      model.mergeAttributes(((Model) flashModel).asMap());
-      return true;
     }
+    model.mergeAttributes(((Model) redirectFlashModelAttribute).asMap());
+    return true;
   }
 
 }
